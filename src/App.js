c7a7c6aa72083c9cf64474fa76ff1511011150df
Jordan <jconheasman@gmail.com>
2020-11-27 20:59:13 -0800

    adjusted readme to include npm as well as yarn. and imported some checckboxes from Material UI for demo purposes.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import logo from './mintbean.png';
+import Checkbox from "@material-ui/core/Checkbox";
+import Checkboxes from './components/checkboxes'
 import './styles/index.css';
 
 function App() {
@@ -12,6 +14,8 @@ function App() {
         <h2>HAPPY HACKING!</h2>
         <h3>MegaBeats</h3>
         <p>get the party started.</p>
+        <p>Just added checkboxes to show how eaasy it is</p>
+        <Checkbox />
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -20,6 +24,7 @@ function App() {
         >
           Learn React
         </a>
+        <Checkboxes />
     </div>
   );
 }
