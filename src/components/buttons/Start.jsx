925fbac0284c5977477f7f11a5393672f1fcb969
Angel <blacitea@gmail.com>
2020-11-30 12:31:48 -0800

    change button display based on state

--- src/components/buttons/Start.jsx
+++ src/components/buttons/Start.jsx
@@ -2,22 +2,48 @@ import React, { useState } from 'react';
 import Button from '@material-ui/core/Button';
 
 import PlayCircleOutlineIcon from '@material-ui/icons/PlayCircleOutline';
+import PauseCircleOutlineIcon from '@material-ui/icons/PauseCircleOutline';
+
+import green from '@material-ui/core/colors/green';
+import {
+	createMuiTheme,
+	ThemeProvider,
+	makeStyles,
+} from '@material-ui/core/styles';
 
 const Start = () => {
 	const [play, setPlay] = useState(false);
 	const handleClick = () => {
 		setPlay(!play);
 	};
-	return (
+	const theme = createMuiTheme({
+		palette: {
+			primary: green,
+		},
+	});
+
+	return play ? (
 		<Button
 			variant="contained"
-			color="primary"
 			size="xlarge"
-			endIcon={<PlayCircleOutlineIcon fontSize="large" />}
+			color="secondary"
+			endIcon={<PauseCircleOutlineIcon fontSize="large" />}
 			onClick={handleClick}
 		>
-			Start
+			Pause
 		</Button>
+	) : (
+		<ThemeProvider theme={theme}>
+			<Button
+				variant="contained"
+				size="xlarge"
+				color="primary"
+				endIcon={<PlayCircleOutlineIcon fontSize="large" />}
+				onClick={handleClick}
+			>
+				Start
+			</Button>
+		</ThemeProvider>
 	);
 };
 
