56d2b09f4bb27bd4fca2c88572559033d8a7b760
Jordan <jconheasman@gmail.com>
2020-11-29 16:33:47 -0800

    created 3 rows of 4 checkboxes for practice using material UI inport for styling

--- src/App.js
+++ src/App.js
@@ -1,30 +1,54 @@
 import React from 'react';
 import logo from './mintbean.png';
-import Checkbox from "@material-ui/core/Checkbox";
-import Checkboxes from './components/checkboxes'
+// import Checkboxes from './components/checkboxes'
+import { Checkbox } from "@material-ui/core";
 import './styles/index.css';
 
 function App() {
   return (
     <div className="App">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          <code>App.js</code> is the entrypoint to your app.
-        </p>
-        <h2>HAPPY HACKING!</h2>
-        <h3>MegaBeats</h3>
-        <p>get the party started.</p>
-        <p>Just added checkboxes to show how eaasy it is</p>
-        <Checkbox />
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <Checkboxes />
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        <code>App.js</code> is the entrypoint to your app.
+      </p>
+      <h2>HAPPY HACKING!</h2>
+      <h3>MegaBeats</h3>
+      <p>Click the link below to see the code in full</p>
+      <a
+        className="App-link"
+        href="https://github.com/blacitea/MegaBeats"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        GitHub Repo
+      </a>
+      <div>
+          Symbols
+        <>
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+        </>
+      </div>
+      <div>
+          High-Hat
+        <>
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+        </>
+      </div>
+      <div>
+          Snare
+        <>
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+          <Checkbox color="primary" />
+        </>
+      </div>
     </div>
   );
 }
--- src/components/checkboxes.jsx
+++ src/components/checkboxes.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import Checkbox from '@material-ui/core/Checkbox';
 
 export default function Checkboxes() {
-  const [checked, setChecked] = React.useState(true);
+  const [checked, setChecked] = React.useState(false);
 
   const handleChange = (event) => {
     setChecked(event.target.checked);
@@ -13,31 +13,20 @@ export default function Checkboxes() {
       <Checkbox
         checked={checked}
         onChange={handleChange}
-        inputProps={{ 'aria-label': 'primary checkbox' }}
-      />
-      <Checkbox
-        defaultChecked
         color="primary"
         inputProps={{ 'aria-label': 'secondary checkbox' }}
+        value="Symbol"
+        label="Start"
       />
-      <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
-      <Checkbox disabled inputProps={{ 'aria-label': 'disabled checkbox' }} />
-      <Checkbox disabled checked inputProps={{ 'aria-label': 'disabled checked checkbox' }} />
-      <Checkbox
-        defaultChecked
-        indeterminate
-        inputProps={{ 'aria-label': 'indeterminate checkbox' }}
+      <Checkbox 
+        color="primary"
       />
-      <Checkbox
-        defaultChecked
-        color="default"
-        inputProps={{ 'aria-label': 'checkbox with default color' }}
+      <Checkbox 
+        color="primary"
       />
-      <Checkbox
-        defaultChecked
-        size="small"
-        inputProps={{ 'aria-label': 'checkbox with small size' }}
-      />
+      <Checkbox 
+        color="primary"
+      />      
     </div>
   );
 }
